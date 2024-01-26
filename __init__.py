b343b6afcae0cc67e0346725b3b304e4c87642d3
asinpk <ivan.a.kuzin@gmail.com>
2024-01-26 12:19:48 +1200

    Update copyright

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
-# Copyright (c) 2022 Aldo Hoeben / fieldOfView
+# Copyright (c) 2024 Ivan Kuzin
+# Code based on LinearAdvanceSettingsPlugin by Aldo Hoeben / fieldOfView
 # The FilamentRunoutSettingsPlugin is released under the terms of the AGPLv3 or higher.
 
 from . import FilamentRunoutSensorSettingsPlugin
